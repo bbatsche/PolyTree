78163fe6e2a550b51bbce2b41863392b7fc564d0
Ben Batschelet <bbatsche@users.noreply.github.com>
2016-06-05 14:51:27 -0500

    Testing for attachAncestry exit conditions
    Well, these are some crazy brittle tests...

--- tests/Relations/IndirectTest.php
+++ tests/Relations/IndirectTest.php
@@ -47,6 +47,120 @@ class IndirectTest extends TestCase
 
         $relation->detach('test');
     }
+
+    public function testAttachAncestryThrowsLockedException()
+    {
+        $parent = Mockery::mock('BeBat\PolyTree\Model');
+        $child  = Mockery::mock('BeBat\PolyTree\Model');
+
+        // Yes we're mocking the supposed SUT. Because we're passing the mocked SUT to the REAL SUT.
+        $relation = Mockery::mock('BeBat\PolyTree\Relations\Indirect');
+
+        $relation->shouldReceive('isLocked')->withNoArgs()->andReturn(true)->once();
+
+        $this->setExpectedException('BeBat\PolyTree\Exceptions\LockedRelationship');
+
+        IndirectBase::attachAncestry($parent, $child, $relation);
+    }
+
+    public function testAttachAncestryThrowsCycleExceptionForChild()
+    {
+        $parent = Mockery::mock('BeBat\PolyTree\Model');
+        $child  = Mockery::mock('BeBat\PolyTree\Model');
+
+        // Yes we're mocking the supposed SUT. Because we're passing the mocked SUT to the REAL SUT.
+        $relation = Mockery::mock('BeBat\PolyTree\Relations\Indirect');
+
+        $mockPivot = Mockery::mock('Pivot');
+
+        $relation->shouldReceive('isLocked')->withNoArgs()->andReturn(false)->once();
+
+        $parent->shouldReceive('getKey')->withNoArgs()->andReturn('parent_key')->once();
+
+        $child->shouldReceive('hasDescendants->newPivotStatementForId')->with('parent_key')->andReturn($mockPivot)->once();
+
+        $mockPivot->shouldReceive('count')->withNoArgs()->andReturn(1)->once();
+
+        $this->setExpectedException('BeBat\PolyTree\Exceptions\Cycle');
+
+        IndirectBase::attachAncestry($parent, $child, $relation);
+    }
+
+    public function testAttachAncestryThrowsCycleExceptionForParent()
+    {
+        $parent = Mockery::mock('BeBat\PolyTree\Model');
+        $child  = Mockery::mock('BeBat\PolyTree\Model');
+
+        // Yes we're mocking the supposed SUT. Because we're passing the mocked SUT to the REAL SUT.
+        $relation = Mockery::mock('BeBat\PolyTree\Relations\Indirect');
+
+        $mockPivot = Mockery::mock('Pivot');
+
+        $relation->shouldReceive('isLocked')->withNoArgs()->andReturn(false)->once();
+
+        $parent->shouldReceive('getKey')->withNoArgs()->andReturn('parent_key')->once();
+        $child->shouldReceive('getKey')->withNoArgs()->andReturn('child_key')->once();
+
+        $parent->shouldReceive('hasAncestors->newPivotStatementForId')->with('child_key')->andReturn($mockPivot)->once();
+        $child->shouldReceive('hasDescendants->newPivotStatementForId')->with('parent_key')->andReturn($mockPivot)->once();
+
+        $mockPivot->shouldReceive('count')->withNoArgs()->andReturn(0, 1)->twice();
+
+        $this->setExpectedException('BeBat\PolyTree\Exceptions\Cycle');
+
+        IndirectBase::attachAncestry($parent, $child, $relation);
+    }
+
+    public function testAttachAncestryDoesNothingForChild()
+    {
+        $parent = Mockery::mock('BeBat\PolyTree\Model');
+        $child  = Mockery::mock('BeBat\PolyTree\Model');
+
+        // Yes we're mocking the supposed SUT. Because we're passing the mocked SUT to the REAL SUT.
+        $relation = Mockery::mock('BeBat\PolyTree\Relations\Indirect');
+
+        $mockPivot = Mockery::mock('Pivot');
+
+        $relation->shouldReceive('isLocked')->withNoArgs()->andReturn(false)->once();
+
+        $parent->shouldReceive('getKey')->withNoArgs()->andReturn('parent_key')->twice();
+        $child->shouldReceive('getKey')->withNoArgs()->andReturn('child_key')->once();
+
+        $parent->shouldReceive('hasAncestors->newPivotStatementForId')->with('child_key')->andReturn($mockPivot)->once();
+        $child->shouldReceive('hasDescendants->newPivotStatementForId')->with('parent_key')->andReturn($mockPivot)->once();
+
+        $child->shouldReceive('hasAncestors->newPivotStatementForId')->with('parent_key')->andReturn($mockPivot)->once();
+
+        $mockPivot->shouldReceive('count')->withNoArgs()->andReturn(0, 0, 1)->times(3);
+
+        verify(IndirectBase::attachAncestry($parent, $child, $relation))->isEmpty();
+    }
+
+    public function testAttachAncestryDoesNothingForParent()
+    {
+        $parent = Mockery::mock('BeBat\PolyTree\Model');
+        $child  = Mockery::mock('BeBat\PolyTree\Model');
+
+        // Yes we're mocking the supposed SUT. Because we're passing the mocked SUT to the REAL SUT.
+        $relation = Mockery::mock('BeBat\PolyTree\Relations\Indirect');
+
+        $mockPivot = Mockery::mock('Pivot');
+
+        $relation->shouldReceive('isLocked')->withNoArgs()->andReturn(false)->once();
+
+        $parent->shouldReceive('getKey')->withNoArgs()->andReturn('parent_key')->twice();
+        $child->shouldReceive('getKey')->withNoArgs()->andReturn('child_key')->twice();
+
+        $parent->shouldReceive('hasAncestors->newPivotStatementForId')->with('child_key')->andReturn($mockPivot)->once();
+        $child->shouldReceive('hasDescendants->newPivotStatementForId')->with('parent_key')->andReturn($mockPivot)->once();
+
+        $child->shouldReceive('hasAncestors->newPivotStatementForId')->with('parent_key')->andReturn($mockPivot)->once();
+        $parent->shouldReceive('hasDescendants->newPivotStatementForId')->with('child_key')->andReturn($mockPivot)->once();
+
+        $mockPivot->shouldReceive('count')->withNoArgs()->andReturn(0, 0, 0, 1)->times(4);
+
+        verify(IndirectBase::attachAncestry($parent, $child, $relation))->isEmpty();
+    }
 }
 
 class Indirect extends IndirectBase {}
