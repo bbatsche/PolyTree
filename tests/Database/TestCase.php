b5e6a28b377141f87ae94131d28aa6ee024eefc5
Ben Batschelet <bbatsche@users.noreply.github.com>
2016-06-17 10:35:53 -0500

    Some utility functions to ease testing DB values. "Base" fixtures which can be added to dynamically in test

--- tests/Database/TestCase.php
+++ tests/Database/TestCase.php
@@ -5,15 +5,21 @@ namespace BeBat\PolyTree\Test\Database;
 use BeBat\PolyTree\Test\ArrayDataSet;
 use Illuminate\Database\Capsule\Manager;
 use PHPUnit_Extensions_Database_TestCase as TestCaseBase;
+use PHPUnit_Extensions_Database_DataSet_CompositeDataSet as CompositeDataSet;
+use PHPUnit_Extensions_Database_DataSet_IDataSet as DataSetInterface;
 use PHPUnit_Extensions_Database_DataSet_YamlDataSet as YamlDataSet;
 
 abstract class TestCase extends TestCaseBase
 {
     protected static $fixturePrefix;
 
+    protected static $baseFixtures = array();
+
     public static function setupBeforeClass()
     {
         static::$fixturePrefix = realpath(__DIR__ . '/../_fixtures');
+
+        static::$baseFixtures = array('TestModels.yml');
     }
 
     public function getConnection()
@@ -24,7 +30,13 @@ abstract class TestCase extends TestCaseBase
 
     public function getDataSet()
     {
-        return $this->createYamlDataSet('TestModelTable.yml');
+        $compositeSet = new CompositeDataSet();
+
+        foreach (static::$baseFixtures as $filename) {
+            $compositeSet->addDataSet($this->createYamlDataSet($filename));
+        }
+
+        return $compositeSet;
     }
 
     public function createYamlDataSet($fileName)
@@ -40,5 +52,37 @@ abstract class TestCase extends TestCaseBase
     public function createArrayDataSet(array $data)
     {
         return new ArrayDataSet($data);
+    }
+
+    public function appendDataSet(DataSetInterface $newData)
+    {
+        $compositeSet = new CompositeDataSet();
+
+        foreach (static::$baseFixtures as $filename) {
+            $compositeSet->addDataSet($this->createYamlDataSet($filename));
+        }
+
+        $compositeSet->addDataSet($newData);
+
+        $this->getDatabaseTester()->setDataSet($compositeSet);
+
+        // Run databaseTester's onSetUp to reload DB with our new data
+        $this->getDatabaseTester()->onSetUp();
+    }
+
+    public function createTableFromArray($tableName, $rows)
+    {
+        return $this->createSimpleArrayDataSet($tableName, $rows)->getTable($tableName);
+    }
+
+    public function getActualTableValues($tableName, array $orderBy = [], $dir = 'ASC', array $columns = ['*'])
+    {
+        $sql = 'SELECT ' . implode(',', $columns) . ' FROM ' . $tableName;
+
+        if (!empty($orderBy)) {
+            $sql .= ' ORDER BY ' . implode(',', $orderBy) . ' ' . $dir;
+        }
+
+        return $this->getConnection()->createQueryTable($tableName, $sql);
     }
 }
